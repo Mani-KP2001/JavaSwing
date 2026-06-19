8a70dc33f3e7507a20f8432d27c0c3c2d8284c40
Mani-KP2001 <kpmani200165@gmail.com>
2026-06-19 18:19:46 +0545

    Refactor Authentication class for clarity and structure

--- Authentication.java
+++ Authentication.java
@@ -1,7 +1,6 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-//login and register switching panel example
 public class Authentication implements ActionListener {
     JPanel container = new JPanel();
     CardLayout card = new CardLayout();
